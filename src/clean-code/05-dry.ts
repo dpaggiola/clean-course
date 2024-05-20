bddf3c6928cf73797c00d0218161f72ad0253cce
Daniel Paggiola <dpaggiola@gmail.com>
2024-05-20 13:00:01 -0300

    Clase 22

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,17 +7,38 @@ class Product {
         public size: Size = '',
     ) {}
 
+    isProductReady(): boolean {
+        for (const key in this ) {
+            switch(typeof this[key]) {
+                 case 'string':
+                     if ((<string>this[key]).length <= 0)
+                         throw Error(`${ key } is empty`);
+                 break;
+                 case 'number':
+                     if ((<number>this[key]) <= 0)
+                         throw Error(`${ key } is zero`);
+                 break;
+                 default:
+                    throw Error(`${typeof this[key]} is not valid`);
+                    
+            }
+        }
+        return true;
+    }
+
     toString() {
         //No DRY
-        if (this.name.length <= 0) throw Error('Name is empty');
-        if (this.price <= 0) throw Error('Price is zero');
-        if (this.size.length <= 0) throw Error('Size is empty');
+        // if (this.name.length <= 0) throw Error('Name is empty');
+        // if (this.price <= 0) throw Error('Price is zero');
+        // if (this.size.length <= 0) throw Error('Size is empty');
+
+        if ( !this.isProductReady) return;
 
         return `${this.name} (${this.price}), $${this.size}`;
     }
 }
 
 (()=> {
-    const bluePants = new Product('Blue large pants', 10, 'M');
+    const bluePants = new Product('Blue large pants', 10, 'S');
     console.log(bluePants.toString());
 })
